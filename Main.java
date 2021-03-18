76d2918f8b3b495d480aca3910fdc633c3053715
hakim <hakim.ofkir@outlook.fr>
2021-03-18 01:21:16 +0100

    triger

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Hakim ca va!!!!");
+        System.out.println("Hello, Hakim ca va test triger jenkins hhhhhhhhhhhhhhhhhhhhhh!!!!");
     }
 }
 
