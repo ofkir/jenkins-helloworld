6584b1045234fbdb9b0e9e6b169219807ad8db3d
hakim <hakim.ofkir@outlook.fr>
2021-03-20 12:09:20 +0100

    Test MonPremierPipeline

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Hakim ceci c'est un test !!"); 
+        System.out.println("Hello, Hakim, test MonPremierPipeline   !!"); 
 
     }
 }
